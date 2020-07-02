2aaa32050036dbeb95c74a55ec31f57aeb9a5638
Hervey <hervenesta@gmail.com>
2020-07-01 21:51:14 -0400

    Find Project by Identifier

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -4,9 +4,10 @@ import io.agileintelligence.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Project findByProjectIdentifier(String projectId);
+
 }
--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -6,6 +6,8 @@ import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProjectService {
 
@@ -21,5 +23,15 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier()+"' already exists");
         }
 
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' doesn't exist");
+        }
+        return project;
     }
 }
