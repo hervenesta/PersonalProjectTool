efc76c729133d63cffb455f46066b846d3c5cca7
Hervey <hervenesta@gmail.com>
2020-07-07 22:34:36 -0400

    get project final step

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
-import projectReducer from "./projectReducer.JS";
+import projectReducer from "./projectReducer.js";
 
 export default combineReducers({
   errors: errorReducer,
